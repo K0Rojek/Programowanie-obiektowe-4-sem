2ccfba70014dd53c8efa511196d5735fe049fc5a
Kamil Rojek <kamilrojek2002@gmail.com>
2023-05-04 19:27:54 +0200

    introducing money and wallet to ticketmachine

--- src/TicketSeller/TicketMachine.java
+++ src/TicketSeller/TicketMachine.java
@@ -11,5 +11,24 @@ public class TicketMachine {
             new Ticket("Reduced", 2.00), new Ticket("Normal", 3.00)
     };
     private ArrayList<Transaction> soldTickets = new ArrayList<>();
-    //private ;
+    private Money[] money = new Money[] {
+            new Bill200(),
+            new Bill100(),
+            new Bill50(),
+            new Bill20(),
+            new Bill10(),
+            new Coin5(),
+            new Coin2(),
+            new Coin1(),
+            new Coin0_50(),
+            new Coin0_20(),
+            new Coin0_10(),
+            new Coin0_05(),
+            new Coin0_02(),
+            new Coin0_01()
+    };
+
+    public TicketMachine(String ticketMachineLocation) {
+        this.ticketMachineLocation = ticketMachineLocation;
+    }
 }
